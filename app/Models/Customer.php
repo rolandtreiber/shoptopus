8e6de626b71db91d7e27d069fa66b066d2dfa651
Roland Treiber <hello@thecaringdeveloper.com>
2022-04-25 06:22:33 +0100

    Customer model

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\UserRole;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+
+class Customer extends User {
+
+    public static function all($columns = ['*'])
+    {
+        $role = Role::findByName(UserRole::Customer);
+        $result = DB::table('users')
+            ->join('model_has_roles', 'users.id', 'model_has_roles.model_id')
+            ->where('model_has_roles.model_type', "App\Models\User")
+            ->where('model_has_roles.role_id', $role->id)
+            ->get();
+
+        return self::hydrate($result->toArray());
+    }
+}
