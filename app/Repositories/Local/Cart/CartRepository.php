ab2392ee141ee28390f666cc3093233fd94b16fa
Istvan Lovas <hello@istvanlovas.co.uk>
2022-05-16 20:58:09 +0100

    Fix cart bug

--- app/Repositories/Local/Cart/CartRepository.php
+++ app/Repositories/Local/Cart/CartRepository.php
@@ -24,7 +24,7 @@ class CartRepository extends ModelRepository implements CartRepositoryInterface
     {
         try {
             $cart = $payload['cart_id']
-                ? $this->get(value: $payload['cart_id'], excludeRelationships: ['user'])
+                ? $this->get($payload['cart_id'])
                 : $this->post([]);
 
             $cart_product_table = DB::table('cart_product');
@@ -48,7 +48,7 @@ class CartRepository extends ModelRepository implements CartRepositoryInterface
                 ]);
             }
 
-            return $this->get(value: $payload['cart_id'], excludeRelationships: ['user']);
+            return $this->get(value: $cart['id'], excludeRelationships: ['users']);
         } catch (\Exception | \Error $e) {
             $this->errorService->logException($e);
             throw $e;
