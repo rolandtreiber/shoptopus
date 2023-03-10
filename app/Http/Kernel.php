7e105d1c3ffecee40f78fbce33f13c3de199493a
Shift <shift@laravelshift.com>
2023-03-10 11:35:19 +0000

    Remove `fruitcake/laravel-cors` dependency

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ use App\Http\Middleware\SuperUser;
 use App\Http\Middleware\TrimStrings;
 use App\Http\Middleware\TrustProxies;
 use App\Http\Middleware\VerifyCsrfToken;
-use Fruitcake\Cors\HandleCors;
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Auth\Middleware\AuthenticateWithBasicAuth;
 use Illuminate\Auth\Middleware\Authorize;
 use Illuminate\Auth\Middleware\RequirePassword;
