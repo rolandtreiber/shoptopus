1b7ba18a55e64dab466dcf665333dfa410e7a04c
Shift <shift@laravelshift.com>
2023-05-30 05:38:11 +0000

    Shift core files

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -16,15 +16,6 @@ class Handler extends ExceptionHandler
     use APIControllerTrait;
 
     /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Traits\APIControllerTrait;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, APIControllerTrait;
+    use AuthorizesRequests, ValidatesRequests, APIControllerTrait;
 }
