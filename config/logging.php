e6684592fd9a0698ade1ea4d0c97969b63f303d2
Shift <shift@laravelshift.com>
2023-05-30 05:38:13 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them and merged your true customizations -
    where ENV variables may not be used.

--- config/logging.php
+++ config/logging.php
@@ -1,11 +1,9 @@
 <?php
 
-use Elasticsearch\ClientBuilder;
-use Monolog\Formatter\ElasticsearchFormatter;
-use Monolog\Handler\ElasticsearchHandler;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Processor\PsrLogMessageProcessor;
 
 return [
 
@@ -67,6 +65,7 @@ return [
                 'client' => config('app.env') !== 'testing' ? ClientBuilder::create()->setHosts([env('ELASTIC_SCHEME').'://'.env('ELASTIC_HOST').':'.env('ELASTIC_PORT')])->build() : null,
             ],
         ],
+
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single', 'elasticsearch'],
@@ -77,6 +76,7 @@ return [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
         ],
 
         'daily' => [
@@ -84,6 +84,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
+            'replace_placeholders' => true,
         ],
 
         'slack' => [
@@ -92,6 +93,7 @@ return [
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
             'level' => env('LOG_LEVEL', 'critical'),
+            'replace_placeholders' => true,
         ],
 
         'papertrail' => [
@@ -103,6 +105,7 @@ return [
                 'port' => env('PAPERTRAIL_PORT'),
                 'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
+            'processors' => [PsrLogMessageProcessor::class],
         ],
 
         'stderr' => [
@@ -113,16 +116,20 @@ return [
             'with' => [
                 'stream' => 'php://stderr',
             ],
+            'processors' => [PsrLogMessageProcessor::class],
         ],
 
         'syslog' => [
             'driver' => 'syslog',
             'level' => env('LOG_LEVEL', 'debug'),
+            'facility' => LOG_USER,
+            'replace_placeholders' => true,
         ],
 
         'errorlog' => [
             'driver' => 'errorlog',
             'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
         ],
 
         'null' => [
