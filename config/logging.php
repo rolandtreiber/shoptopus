30b9448c19c4d22112516a1239135b9497b2a962
Roland Treiber <hello@thecaringdeveloper.com>
2023-07-25 14:37:28 +0100

    Elastic logging removed as it fails the pipeline

--- config/logging.php
+++ config/logging.php
@@ -55,6 +55,20 @@ return [
     */
 
     'channels' => [
+//        'elasticsearch' => [
+//            'driver' => 'monolog',
+//            'level' => 'debug',
+//            'handler' => ElasticsearchHandler::class,
+//            'formatter' => ElasticsearchFormatter::class,
+//            'formatter_with' => [
+//                'index' => env('ELASTIC_LOGS_INDEX'),
+//                'type' => '_doc',
+//            ],
+//            'handler_with' => [
+//                'client' => config('app.env') !== 'testing' ? ClientBuilder::create()->setHosts([env('ELASTIC_SCHEME').'://'.env('ELASTIC_HOST').':'.env('ELASTIC_PORT')])->build() : null,
+//            ],
+//        ],
+//
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
