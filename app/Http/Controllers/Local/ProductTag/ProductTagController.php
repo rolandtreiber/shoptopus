2eabceba3c4a33748aadce15ff49e31e226ecd84
Roland Treiber <info@thecaringdeveloper.com>
2024-05-28 15:42:48 +0100

    Controller logic

--- app/Http/Controllers/Local/ProductTag/ProductTagController.php
+++ app/Http/Controllers/Local/ProductTag/ProductTagController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\Local\ProductTag;
 
 use App\Http\Controllers\Controller;
+use App\Services\Local\ProductTag\ProductTagServiceInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductTagController extends Controller
 {
-    //
+    private ProductTagServiceInterface $productTagService;
+
+    public function __construct(ProductTagServiceInterface $productTagService)
+    {
+        $this->productTagService = $productTagService;
+    }
+
+    /**
+     * Get all models
+     */
+    public function getAll(Request $request): JsonResponse
+    {
+        try {
+            [$filters, $page_formatting] = $this->getFiltersAndPageFormatting($request);
+            return response()->json($this->getResponse(
+                $page_formatting,
+                $this->productTagService->getAll($page_formatting, $filters),
+                $request
+            ));
+        } catch (\Exception|\Error $e) {
+            return $this->errorResponse($e, __('error_messages.'.$e->getCode()));
+        }
+    }
+
+    /**
+     * Get a single model
+     */
+    public function get(Request $request, string $id): JsonResponse
+    {
+        try {
+            return response()->json($this->getResponse([], $this->productTagService->get($id), $request));
+        } catch (\Exception|\Error $e) {
+            return $this->errorResponse($e, __('error_messages.'.$e->getCode()));
+        }
+    }
 }
---
+++ routes/api/product_tag/product_tag.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\Admin\ProductTagController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'name' => 'api.'
+], function () {
+    Route::group([
+        'name' => 'product_tags.',
+        'prefix' => 'product_tags'
+    ], function () {
+        Route::get('/', [ProductTagController::class, 'getAll'])->name('api.product_tags.getAll');
+    });
+
+    Route::group([
+        'name' => 'product_tag.',
+        'prefix' => 'product_tag'
+    ], function () {
+        Route::get('/{id}', [ProductTagController::class, 'get'])->name('api.product_tag.get');
+    });
+});
