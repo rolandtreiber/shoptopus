dbc298ca617e3251458199624d58332494993859
Roland Treiber <hello@thecaringdeveloper.com>
2024-10-01 16:57:14 +0100

    Homepage api works

--- app/Repositories/Local/HomePage/HomePageRepository.php
+++ app/Repositories/Local/HomePage/HomePageRepository.php
@@ -2,20 +2,17 @@
 
 namespace App\Repositories\Local\HomePage;
 
-use App\Enums\DiscountType;
 use App\Enums\ProductStatus;
 use App\Http\Resources\HomePage\ProductResource;
 use App\Models\Product;
 use App\Models\User;
 use App\Repositories\Local\Product\ProductRepositoryInterface;
-use App\Services\Local\Product\ProductServiceInterface;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class HomePageRepository implements HomePageRepositoryInterface
 {
-    private User $user;
-
+    private User|null $user;
     private ProductRepositoryInterface $productRepository;
 
     public function __construct(ProductRepositoryInterface $productRepository)
@@ -29,7 +26,8 @@ class HomePageRepository implements HomePageRepositoryInterface
         $highlightedProducts = $this->getHighlightedProducts();
         return [
             'banners' => $this->getBanners(),
-            'highlighted_products' => ProductResource::collection(Product::whereIn('id', $highlightedProducts)->get()),
+            'highlighted_categories' => $this->getHighlightedCategories(),
+            'highlighted_products' => ProductResource::collection(Product::whereIn('id', $highlightedProducts)->get())
         ];
     }
 
@@ -37,7 +35,7 @@ class HomePageRepository implements HomePageRepositoryInterface
     {
         $banners = DB::table('banners')->where('enabled', 1)->select('title', 'description', 'background_image', 'show_button', 'button_text', 'button_url', 'enabled')->get();
 
-        return $banners->map(function($banner) {
+        return $banners->map(function ($banner) {
             return [
                 'title' => json_decode($banner['title']),
                 'description' => json_decode($banner['description']),
@@ -50,7 +48,7 @@ class HomePageRepository implements HomePageRepositoryInterface
         })->toArray();
     }
 
-    private function getHighlightedProducts()
+    private function getHighlightedProducts(): array
     {
         $productIdsWithHighSalesVolume = DB::table('products')
             ->orderBy('purchase_count', 'desc')
@@ -60,34 +58,38 @@ class HomePageRepository implements HomePageRepositoryInterface
 
         $discountedProductsByCategory = DB::table('discount_rule_product_category')->join('discount_rules', function ($join) {
             $join->on('discount_rule_product_category.discount_rule_id', '=', 'discount_rules.id')->where('discount_rules.enabled', '=', 1);
-        })->join('product_product_category', function($join) {
+        })->join('product_product_category', function ($join) {
             $join->on('discount_rule_product_category.product_category_id', '=', 'product_product_category.product_category_id');
         })->join('products', function ($join) {
             $join->on('products.id', '=', 'product_product_category.product_id')
                 ->where('products.status', '=', ProductStatus::Active)
-                ->where('products.stock', '>', 0)
-            ;
+                ->where('products.stock', '>', 0);
         })
             ->select('product_id')->limit(50)->pluck('product_id')->toArray();
 
         $discountedProductsDirectly = DB::table('discount_rule_product')->join('discount_rules', function ($join) {
-            $join->on('discount_rule_product.discount_rule_id', '=', 'discount_rules.id')->where('discount_rules.enabled', '=', 1);
+            $join->on('discount_rule_product.discount_rule_id', '=', 'discount_rules.id')
+                ->where(function ($query) {
+                    $query->where('discount_rules.valid_from', '<', Carbon::now())
+                        ->where('discount_rules.valid_until', '>', Carbon::now());
+                })->orWhere(function ($query) {
+                    $query->where('discount_rules.valid_from', '<', Carbon::now())
+                        ->where('discount_rules.valid_until', null);
+                })->where('discount_rules.enabled', '=', 1);
         })->join('products', function ($join) {
             $join->on('products.id', '=', 'discount_rule_product.product_id')
                 ->where('products.status', '=', ProductStatus::Active)
-                ->where('products.stock', '>', 0)
-            ;
+                ->where('products.stock', '>', 0);
         })->select('discount_rule_product.product_id')->limit(50)->pluck('product_id')->toArray();
 
         $likedProducts = [];
         if ($this->user) {
             $likedProducts = DB::table('favorited_products')->where('user_id', $this->user->id)
                 ->join('products', function ($join) {
-                $join->on('products.id', '=', 'favorited_products.product_id')
-                    ->where('products.status', '=', ProductStatus::Active)
-                    ->where('products.stock', '>', 0)
-                ;
-            })->pluck('product_id')->toArray();
+                    $join->on('products.id', '=', 'favorited_products.product_id')
+                        ->where('products.status', '=', ProductStatus::Active)
+                        ->where('products.stock', '>', 0);
+                })->pluck('product_id')->toArray();
         }
 
         $allDisplayable = array_merge($productIdsWithHighSalesVolume, $discountedProductsByCategory, $discountedProductsDirectly, $likedProducts);
@@ -103,5 +105,107 @@ class HomePageRepository implements HomePageRepositoryInterface
 
         shuffle($allDisplayable);
         return $allDisplayable;
+    }
+
+    private function getHighlightedCategories()
+    {
+        $highlightedCategoryIds = DB::table('discount_rule_product_category')
+            ->join('discount_rules', function ($join) {
+                $join->on('discount_rule_product_category.discount_rule_id', '=', 'discount_rules.id')
+                    ->where(function ($query) {
+                        $query->where('discount_rules.valid_from', '<', Carbon::now())
+                            ->where('discount_rules.valid_until', '>', Carbon::now());
+                    })->orWhere(function ($query) {
+                        $query->where('discount_rules.valid_from', '<', Carbon::now())
+                            ->where('discount_rules.valid_until', null);
+                    });
+            })
+            ->where('enabled', 1)
+            ->pluck('product_category_id')->toArray();
+
+        $missing = 5 - count($highlightedCategoryIds);
+        if ($missing > 0) {
+            $remainingCategoryIds = DB::table('product_categories')
+                ->where('enabled', 1)
+                ->whereNotIn('id', $highlightedCategoryIds)
+                ->pluck('id')
+                ->toArray();
+            shuffle($remainingCategoryIds);
+            $highlightedCategoryIds = array_merge($highlightedCategoryIds, array_slice($remainingCategoryIds, 0, $missing));
+        }
+
+        $result = [];
+
+        $items = DB::table('product_categories')
+            ->whereIn('product_categories.id', $highlightedCategoryIds)
+            ->join('product_product_category', function ($join) {
+                $join->on('product_product_category.product_category_id', '=', 'product_categories.id');
+            })
+            ->join('products', function ($join) {
+                $join->on('products.id', '=', 'product_product_category.product_id')
+                    ->where('products.status', ProductStatus::Active)
+                    ->where('products.stock', '>', 0)
+                    ->orderBy('products.purchase_count', 'desc');
+            })
+            ->get([
+                'product_categories.name as category_name',
+                'product_categories.description as category_description',
+                'product_categories.header_image as category_header_image',
+                'product_categories.id as category_id',
+                'products.id as bestseller_id',
+                'products.name as product_name',
+                'products.slug as product_slug',
+                'products.price as product_price',
+                'products.stock as product_stock',
+                'products.purchase_count as product_purchase_count',
+                'products.short_description as product_short_description',
+                'products.description as product_description',
+                'products.cover_photo as product_cover_photo',
+            ]);
+
+        foreach ($items as $item) {
+            if (array_key_exists($item['category_id'], $result) && count($result[$item['category_id']]['best_sellers']) < 3) {
+                $result[$item['category_id']]['best_sellers'][] = [
+                    'id' => $item['bestseller_id'],
+                    'name' => json_decode($item['product_name']),
+                    'slug' => $item['product_slug'],
+                    'price' => $item['product_price'],
+                    'stock' => $item['product_stock'],
+                    'purchase_count' => $item['product_purchase_count'],
+                    'short_description' => json_decode($item['product_short_description']),
+                    'description' => json_decode($item['product_description']),
+                    'cover_photo' => json_decode($item['product_cover_photo'])
+                ];
+            } else {
+                $result[$item['category_id']] = [
+                    'name' => json_decode($item['category_name']),
+                    'description' => json_decode($item['category_description']),
+                    'header_image' => json_decode($item['category_header_image']),
+                    'best_sellers' => [
+                        [
+                            'id' => $item['bestseller_id'],
+                            'name' => json_decode($item['product_name']),
+                            'slug' => $item['product_slug'],
+                            'price' => $item['product_price'],
+                            'stock' => $item['product_stock'],
+                            'purchase_count' => $item['product_purchase_count'],
+                            'short_description' => json_decode($item['product_short_description']),
+                            'description' => json_decode($item['product_description']),
+                            'cover_photo' => json_decode($item['product_cover_photo'])
+                        ]
+                    ],
+                ];
+            }
+        }
+
+        $arrayKeys = array_keys($result);
+        $resultFormatted = [];
+        foreach ($arrayKeys as $arrayKey) {
+            $resultFormatted[] = [
+                'id' => $arrayKey,
+                ...$result[$arrayKey]
+            ];
+        }
+        return $resultFormatted;
     }
 }
