e4027869fb6c174500f841e525fb7b389ca73298
Roland Treiber <rolandtreiber@gmail.com>
2024-10-13 10:52:33 +0100

    Prompt update

--- config/prompts.php
+++ config/prompts.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'optimise-product-text-for-sales' => 'Could you please optimise the following text for increasing sales for e-commerce in the following languages: [TARGET_LANGUAGES], returned in JSON format? [TEXT]'
+    'optimise-product-text-for-sales' => 'Could you please optimise the following text for increasing sales for e-commerce in the following languages: [TARGET_LANGUAGES], returned in JSON format containing only the language ids as keys and the optimised text? [TEXT]'
 ];
